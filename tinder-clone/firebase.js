e3c62cf95dfc53233925dd2158bb6b17bd42cb8d
kshitizsharan <sharan.kshitiz98@gmail.com>
2020-11-15 10:25:26 +0530

    Still showing error on deploying

---
+++ tinder-clone/firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase";
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCxIV5qz_7ZdV-Er0K2rQFJcbvuQp5XiHE",
+    authDomain: "tender-clone-7bf48.firebaseapp.com",
+    databaseURL: "https://tender-clone-7bf48.firebaseio.com",
+    projectId: "tender-clone-7bf48",
+    storageBucket: "tender-clone-7bf48.appspot.com",
+    messagingSenderId: "647628057909",
+    appId: "1:647628057909:web:771fb017ed0c966f36f07c",
+    measurementId: "G-NMGR4F8WJ5"
+  };
+
+
+  const firebaseApp=firebase.initializeApp(firebaseConfig);
+  
